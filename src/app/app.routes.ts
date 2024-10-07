8ea3aba7a99c035e498c9c87784c038121174fe6
Honorius di Gesus <honoriusdigesus@gmail.com>
2024-10-07 12:10:29 -0500

    Personalización del login y el register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import {AuthLayoutComponent} from "./auth/layout/auth-layout/auth-layout.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children:[
+      {
+        path: 'auth/login',
+        loadComponent: () => import('./auth/components/login/login.component').then(c => c.LoginComponent)
+      },
+      {
+        path: 'auth/register',
+        loadComponent: () => import('./auth/components/register/register.component').then(c => c.RegisterComponent)
+      },
+      {
+        path: '',
+        redirectTo: 'auth/login',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: 'product',
+    loadComponent: () => import('./product/layout/product-layout/product-layout.component').then(c => c.ProductLayoutComponent)
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
---
+++ src/app/product/layout/product-layout/product-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-layout.component.html',
+  styleUrl: './product-layout.component.css'
+})
+export class ProductLayoutComponent {
+
+}
